c108ec20066fd800c1637a859e3d21c90b0abff2
Lisa Cook <jemilox@gmail.com>
2016-10-18 10:36:34 -0500

    basic front end angular controller stuff

---
+++ public/scripts/controllers/addHoursController.js
@@ -0,0 +1,4 @@
+myApp.controller('addHoursController', ['$scope', '$http', 'factory', function ($scope, $http, factory){
+  console.log('in addHoursController');
+
+}]);
---
+++ public/scripts/controllers/adminHomeController.js
@@ -0,0 +1,4 @@
+myApp.controller('adminHomeController', ['$scope', '$http', 'factory', function ($scope, $http, factory){
+  console.log('in adminHomeController');
+
+}]);
