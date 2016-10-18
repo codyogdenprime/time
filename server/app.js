b8f32be0824d7e2ab431949dbaaf764e0d4749c7
John Arkema <john.arkema@gmail.com>
2016-10-18 09:51:52 -0500

    index route

--- server/app.js
+++ server/app.js
@@ -3,10 +3,18 @@ var path = require('path');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var pg = require('pg');
 
+app.use(bodyParser.json());
+
+//port decision
 app.listen(process.env.PORT, function() {
     console.log('lisening on', process.env.PORT);
 });
 
+//index route
+var index = require('./routes/index');
 
+//use public folder
 app.use(express.static(path.resolve('public')));
