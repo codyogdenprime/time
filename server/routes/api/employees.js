9180eabb3b9f1b988f2ff9a958929e25b08dfe50
matt <mattcllhn@gmail.com>
2016-10-19 09:33:20 -0500

    post route works

--- server/routes/api/employees.js
+++ server/routes/api/employees.js
@@ -6,7 +6,7 @@ var connectionString = 'postgres://localhost:5432/cimaron-winter';
 
 //selecting all from employees table
 router.get('/employees', function(req, res) {
-  console.log('employees hit');
+  console.log('employees get route hit');
   pg.connect(connectionString, function(err, client, done){
     if(err){
       console.log(err);
@@ -23,12 +23,19 @@ router.get('/employees', function(req, res) {
     }//else
   });//pg.connect
 });//router.get
+
+//add an employee
 router.post('/employees',function(req, res){
+  console.log('employees post route hit');
   var data = req.body;
   console.log('data which is also req.body',data);
-  pg.connect(connectionString, function(){
-    client.query('INSERT INTO employee (empname, isadmin, authid, authpic, authemail) VALUES ($1,$2,$3,$4,$5)',[data.name, data.adminstatus, data.authid, data.authpic, data.authemail]);
+  pg.connect(connectionString, function(err, client, done){
+    if(err){
+      console.log(err);
+    }else {
+    var query = client.query('INSERT INTO employee (empname, isadmin, authid, authpic, authemail) VALUES ($1,$2,$3,$4,$5)',[data.name, data.adminstatus, data.authid, data.authpic, data.authemail]);
     res.sendStatus(201);
+    }//else bracket
   });//pg.connect
 });//post route
 module.exports = router;
