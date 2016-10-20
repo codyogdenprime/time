33d5ad6b2bdbe4780dba672b689dda8d8c71dd56
John Arkema <john.arkema@gmail.com>
2016-10-20 16:00:32 -0500

    clear comments

--- server/routes/firebase.js
+++ server/routes/firebase.js
@@ -7,15 +7,10 @@ var connectionString = 'postgress://localhost:5432/cimarron';
 
 router.get("/dbcheck", function(req, res){
 
-  /* This is where the magic happens. We pull the idtoken off of the request,
-  verify it against our private_key, and then we return the decodedToken */
+
   firebase.auth().verifyIdToken(req.headers.id_token).then(function(decodedToken) {
-    /* Whatever you do in here is protected by your authorization.
-    WARNING: So far you are returning secret data to ANYONE who is logged in
-    there is still more work to be done if you want to implement roles
-    You can use the decodedToken and some logic to do that. */
     var token = decodedToken;
-    console.log(decodedToken); // Here you can see the information firebase gives you about the user
+    console.log(decodedToken);
     pg.connect(connectionString,function(err,client,done){
       var authid = token.user_id;
       if (err) {
@@ -37,7 +32,7 @@ router.get("/dbcheck", function(req, res){
       // If the id_token isn't right, you end up in this callback function
       res.send("No secret data for you!");
     });//end catch
-  });
+  });//end firebase auth
 });//end router dot get
 
 module.exports = router;
