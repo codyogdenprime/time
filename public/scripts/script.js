0e5fb2d20c0a40d07449577ff39e2112dfd95d40
John Arkema <john.arkema@gmail.com>
2016-10-23 17:01:16 -0500

    routes require login

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -9,23 +9,53 @@ myApp.config(['$routeProvider', function ($routeProvider) {
   }).
   when ('/addHours', {
     templateUrl: "/views/addHours.html",
-    controller: "addHoursController"
+    controller: "addHoursController",
+    resolve: {
+          // controller will not be loaded until $waitForSignIn resolves
+          "firebaseUser": function($firebaseAuthService) {
+            return $firebaseAuthService.$requireSignIn();
+          }
+        }
   }).
   when ('/adminHome', {
     templateUrl: "views/adminHome.html",
-    controller: "adminHomeController"
+    controller: "adminHomeController",
+    resolve: {
+          // controller will not be loaded until $waitForSignIn resolves
+          "firebaseUser": function($firebaseAuthService) {
+            return $firebaseAuthService.$requireSignIn();
+          }
+        }
   }).
   when ('/empManage', {
     templateUrl: "views/empManage.html",
-    controller: "empManageController"
+    controller: "empManageController",
+    resolve: {
+          // controller will not be loaded until $waitForSignIn resolves
+          "firebaseUser": function($firebaseAuthService) {
+            return $firebaseAuthService.$requireSignIn();
+          }
+        }
   }).
   when ('/reports', {
     templateUrl: "views/reports.html",
-    controller: "reportsController"
+    controller: "reportsController",
+    resolve: {
+          // controller will not be loaded until $waitForSignIn resolves
+          "firebaseUser": function($firebaseAuthService) {
+            return $firebaseAuthService.$requireSignIn();
+          }
+        }
   }).
   when ('/userHome', {
     templateUrl: "views/userHome.html",
-    controller: "userHomeController"
+    controller: "userHomeController",
+    resolve: {
+          // controller will not be loaded until $waitForSignIn resolves
+          "firebaseUser": function($firebaseAuthService) {
+            return $firebaseAuthService.$requireSignIn();
+          }
+        }
   }).
   otherwise({
     redirectTo: "/userHome"
