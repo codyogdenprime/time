eb3d44d766bf81a402c764462c4f35dada283721
John Arkema <john.arkema@gmail.com>
2016-10-24 13:12:55 -0500

    firebase included in api routes

--- server/routes/api/projects.js
+++ server/routes/api/projects.js
@@ -1,18 +1,23 @@
 var router = require('express').Router();
 var path = require('path');
 var pg = require ('pg');
-var connectionString = 'postgres://localhost:5432/cimaron-winter';
+var connectionString = 'postgres://localhost:5432/cimarron';
+var firebase = require('firebase');
+
 
 router.route('/projects')
 //selecting all projects who are not admins from employees table
 .get(function(req, res) {
+  //verify idToken sent in headers
+  firebase.auth().verifyIdToken(req.headers.id_token).then(function(decodedToken) {
+    
   console.log('projects get route hit');
   pg.connect(connectionString, function(err, client, done){
     if(err){
       console.log(err);
     }else {
       var resultsArray = [];
-      var queryResults = client.query('SELECT * FROM projects');
+      var queryResults = client.query('SELECT * FROM projects WHERE isactive = TRUE');
       queryResults.on('row', function(row){
         resultsArray.push(row);
       });//on row function
@@ -22,6 +27,11 @@ router.route('/projects')
       });//on end function
     }//else
   });//pg.connect
+}).catch(function(error){
+  console.log(error);
+  // If the id_token isn't right, you end up in this callback function
+  res.send("Sorry your Auth-Token was incorrect");
+});//end catch
 })//router.get
 
 //add an employee
@@ -61,6 +71,7 @@ router.route('/projects')
     }//else
   });//pg.connect
 });//.put route
+
 
 
 module.exports = router;
