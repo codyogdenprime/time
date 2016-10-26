39b379039733794f57bb282d74f372cd265e8ecb
John Arkema <john.arkema@gmail.com>
2016-10-26 14:45:06 -0500

    get Active Only

--- public/scripts/controllers/empManageController.js
+++ public/scripts/controllers/empManageController.js
@@ -11,22 +11,27 @@ myApp.controller('empManageController', ['$scope', '$http', 'factory', function(
             $scope.allEmployees = results.data;
             console.log($scope.allEmployees);
         });
+        //get all Active employees including Admin's 
+        factory.getActiveEmp().then(function(results){
+          $scope.allActiveEmp = results.data;
+          console.log($scope.allActiveEmp, 'All Active Emp');
+        });
     };
 
     //set emp status active/admin
     $scope.empStatus = function() {
-      //grab data from user empId and isactive or isadmin
-      //if checked send adminstatus //if toggles green send activeStatus black activeStatus
-      var objectToSend = {
-        empid: 16, //of selected employee
-        type: 'adminStatus' // set to active or admin based on whats selected
-      };
+        //grab data from user empId and isactive or isadmin
+        //if checked send adminstatus //if toggles green send activeStatus black activeStatus
+        var objectToSend = {
+            empid: 16, //of selected employee
+            type: 'adminStatus' // set to activeStatus or adminStatus based on whats selected
+        };
         //send object to factory http call
-        factory.changeIsAdmin(objectToSend).then(function(results){
-          console.log(results);
+        factory.changeIsAdmin(objectToSend).then(function(results) {
+            console.log(results);
         });
     };
-
+    //run on page load
     $scope.init();
 
 }]);
